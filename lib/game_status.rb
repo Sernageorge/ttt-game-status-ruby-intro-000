789be30dc144ccb75815d53e846fa904cfa6bc21
George Serna <sernageorge@gmail.com>
2021-02-21 03:30:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  if board == WIN_COMBINATIONS.each.any?{|line| line == board}
+  if board == WIN_COMBINATIONS.each.any?{|line|}
     true
   else
     false
