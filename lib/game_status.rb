626006355e9ad3722b6432e82d18b11878ebecfc
George Serna <sernageorge@gmail.com>
2021-02-21 23:06:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,11 +17,19 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  board == WIN_COMBINATIONS.each do |wins|
-    if wins.any? == position_taken?
-      true
-    else
-      false
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    else 
+      false 
     end
   end
 end
