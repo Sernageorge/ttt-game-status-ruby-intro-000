4ccf4d150d9f200f2bf302401b69def5b2b436b0
George Serna <sernageorge@gmail.com>
2021-02-22 03:59:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -57,11 +57,7 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == "X"
-    return "X"
-  elsif won?(board) == "O"
-    return "O"
-  else
-    return nil
+  if won?(board)
+    return board[won?(board)[0]]
   end
 end
