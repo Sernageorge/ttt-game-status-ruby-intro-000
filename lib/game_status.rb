8e38769e46257452fa596ee8f29432be9c62c675
George Serna <sernageorge@gmail.com>
2021-02-22 01:10:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,10 +26,9 @@ def won?(board)
        position_3 = board[win_index_3]
 
        position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
-         return win_combination
-       else
-         false
-       end
+       return win_combination
+     else
+       false
      end
    end
  end
